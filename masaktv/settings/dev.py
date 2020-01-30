12378165d333c0eb868bae5ed80e675aadc252b0
yuto-dev <directx178@gmail.com>
2020-01-30 05:18:01 +0000

    first commit

---
+++ masaktv/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '6f5999yp0ya=tkhxr4(l8f5j&u8yc#1rcktd2+yq!^p_jbr@u#'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
